b97a0d7dbd5290a1d04efce5f05a04b8a78bd3f8
Sneha22888 <25155636@kiit.ac.in>
2026-04-30 15:40:39 +0530

    Primechekcer done

--- Basics/Primechecker.c
+++ Basics/Primechecker.c
@@ -1,8 +1,24 @@
 #include<stdio.h>
+int isPrime(int x){
+    if(x<=1){
+        return 0;
+    }
+    for(int i=2;i<=x/2;i++){
+        if(x%2==0){
+            return 0;
+        }
+    }
+    return 1;
+}
 int main(){
     int num; 
     printf("Enter a number : ");
     scanf("%d",&num);
-    
+    if(isPrime(num)){
+        printf("%d is a prime number.",num);
+    }
+    else{
+        printf("%d is not a prime number.",num);
+    }
     return 0;
 }
